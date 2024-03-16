4bc42d7b205327ff4e88e554c6c2c74d743b1cee
charles lee <15952031402@163.com>
2024-03-16 13:49:37 +0800

    Update send_song.js appver

--- module/send_song.js
+++ module/send_song.js
@@ -2,7 +2,7 @@
 
 module.exports = (query, request) => {
   query.cookie.os = 'ios'
-  query.cookie.appver = '8.20.21'
+  query.cookie.appver = '9.0.40'
   const data = {
     id: query.id,
     msg: query.msg || '',
